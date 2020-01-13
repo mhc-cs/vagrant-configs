f463a4f3da059eec1c62d8126102671b9c02bcd5
cs-mhc <59834553+cs-mhc@users.noreply.github.com>
2020-01-13 10:39:54 -0500

    Update install-extras.sh

--- anaconda+jupyter/install-extras.sh
+++ anaconda+jupyter/install-extras.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # update repos and install extra packages
-apt-get update ;
-apt-get install -y valgrind openjdk-8-jre-headless gcc make ;
+apt-get update -y ;
+apt-get install -y valgrind openjdk-8-jre-headless spim emacs gcc make ;
 apt-get upgrade -y ;
+apt-get autoremove -y ;
