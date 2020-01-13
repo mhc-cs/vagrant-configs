7486a87a8194c00b216a8433f37e195feb45065d
cs-mhc <59834553+cs-mhc@users.noreply.github.com>
2020-01-13 10:24:41 -0500

    Update install-extras.sh

--- generic/install-extras.sh
+++ generic/install-extras.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y valgrind openjdk-8-jre-headless spim emacs
+apt update -y ;
+apt install -y valgrind openjdk-8-jre-headless spim emacs gcc make ;
+apt upgrade -y ;
